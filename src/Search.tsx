4faf17553b061b2eff5fe203c6a88aa7e13b74d0
Aiden McCaughey <a.mccaughey@ulster.ac.uk>
2021-08-16 18:16:36 +0100

    fix typo

--- src/Search.tsx
+++ src/Search.tsx
@@ -6,28 +6,28 @@ export default (props) => {
   return (
       <form id="searchForm">
             <div class="row">
-                <input class="col-10 form-input" value={ search() } placeholder="search...." onclick={(e)=>setSearch(e.target.value)}/>
+                <input class="col-10 form-input" value={ search() } placeholder="search...." onclick={(e)=>setSearch(e.currentTarget.value)}/>
                 <button type="reset" class="btn btn-warning btn-rounded col-1 mx-3" >Clear</button>
             </div> 
             <div class="row mt-2"> 
                 <div class="col-2">
-                    <input type="radio" name="customSearch" value="popular" onclick={(e)=>setSearch(`:${e.target.value}`)}/>
+                    <input type="radio" name="customSearch" value="popular" onclick={(e)=>setSearch(`:${e.currentTarget.value}`)}/>
                     <label for="customSearch">Popular</label>
                 </div>
                 <div class="col-2">
-                    <input type="radio" name="customSearch" value="top" onclick={(e)=>setSearch(`:${e.target.value}`)}/>
+                    <input type="radio" name="customSearch" value="top" onclick={(e)=>setSearch(`:${e.currentTarget.value}`)}/>
                     <label for="customSearch">Top Rated</label>
                 </div>
                 <div class="col-2">
-                    <input type="radio" name="customSearch" value="trending" onclick={(e)=>setSearch(`:${e.target.value}`)} />
+                    <input type="radio" name="customSearch" value="trending" onclick={(e)=>setSearch(`:${e.currentTarget.value}`)} />
                     <label for="customSearch">Trending</label>
                 </div>
                 <div class="col-2">
-                    <input type="radio" name="customSearch" value="playing" onclick={(e)=>setSearch(`:${e.target.value}`)} />
+                    <input type="radio" name="customSearch" value="playing" onclick={(e)=>setSearch(`:${e.currentTarget.value}`)} />
                     <label for="customSearch">Now Playing</label>
                 </div>
                 <div class="col-2">
-                    <input type="radio" name="customSearch" value="upcoming" onclick={(e)=>setSearch(`:${e.target.value}`)} />
+                    <input type="radio" name="customSearch" value="upcoming" onclick={(e)=>setSearch(`:${e.currentTarget.value}`)} />
                     <label for="customSearch">Upcoming</label>
                 </div>
             </div>  
